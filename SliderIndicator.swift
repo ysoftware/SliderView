7aa8c534e9e7872244e315bb87968c966fa06b0a
Yaroslav Erohin <ysoftware@yandex.ru>
2017-02-19 16:42:20 +0300

    Initial commit

---
+++ SliderIndicator.swift
@@ -0,0 +1,120 @@
+//
+//  SliderIndicator.swift
+//  https://github.com/ysoftware
+//
+//  Copyright © 2017 Yaroslav Erohin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class SliderIndicator: UIView, SliderViewIndicator {
+
+    // MARK: - Properties
+
+    /** Lowest value of the indicator. Cannot be higher than `maxValue`. Default value is 0.0. */
+    @IBInspectable var minValue:Float = 0.0 {
+        didSet {
+            if minValue >= maxValue { minValue += 0.1 }
+            reloadViews()
+        }
+    }
+
+    /** Highest value of the indicator. Cannot be lower than `minValue`. Default value is 1.0. */
+    @IBInspectable var maxValue:Float = 1.0 {
+        didSet {
+            if minValue >= maxValue { maxValue -= 0.1 }
+            reloadViews()
+        }
+    }
+
+    /** Current value of the indicator. Default value is `0.5`. */
+    @IBInspectable var value:Float = 0.5 {
+        didSet {
+            if value > maxValue { value = maxValue }
+            else if value < minValue { value = minValue }
+            reloadViews()
+        }
+    }
+
+    /** Relative padding of the indicator. From `0.0` to `1.0`. Default value is `0`. */
+    @IBInspectable var relativePadding:Float = 0 {
+        didSet {
+            if relativePadding >= 1 { relativePadding = 0.99 }
+            else if relativePadding < 0 { relativePadding = 0 }
+            reloadViews()
+        }
+    }
+
+    /** Colour of the indicator. Default value is `black`.
+
+     To set background colour, use property of `UIView` called `backgroundColor`. */
+    @IBInspectable var foregroundColour: UIColor = .white {
+        didSet {
+            foregroundView.backgroundColor = foregroundColour
+        }
+    }
+
+    /** Defines the behaviour of the indicator. Default value is `center`. */
+    var anchor:Anchor = .center { didSet { reloadViews() }}
+
+    /** Behaviour of the SliderIndicator. */
+    enum Anchor {
+        /** Goes from left side all the way to the right. */
+        case left
+        /** Goes from right side all the way to the left. */
+        case right
+        /** Middle point is at the center, `minValue` on the left, `maxValue` is on the right side.  */
+        case center
+    }
+
+    // MARK: - Private
+
+    private let foregroundView = UIView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(foregroundView)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        addSubview(foregroundView)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        reloadViews()
+    }
+
+    func reloadViews() {
+        UIView.animate(withDuration: 0.1,
+                       delay: 0,
+                       usingSpringWithDamping: 0.8,
+                       initialSpringVelocity: 0,
+                       options: .curveEaseOut,
+                       animations: {
+                        self.foregroundView.backgroundColor = self.foregroundColour
+
+                        let dif = self.maxValue - self.minValue
+                        let absoluteValue = abs(self.value)
+                        let width = Float(self.frame.size.width)
+
+                        switch self.anchor {
+                        case .center:
+                            let center = width / 2
+                            let indicatorWidth = center - (center * self.relativePadding)
+                            let position = indicatorWidth / dif * 2 * absoluteValue
+
+                            self.foregroundView.frame = CGRect(x: CGFloat(self.value >= 0 ? center : center - position), y: 0, width: CGFloat(position), height: self.frame.size.height)
+                        case .left, .right:
+                            let indicatorWidth = width - (width * self.relativePadding)
+                            let position = indicatorWidth / dif * (self.maxValue + self.value)
+
+                            self.foregroundView.frame = CGRect(x: self.anchor == .left ? 0 : CGFloat(width - position), y: 0, width: CGFloat(position), height: self.frame.size.height)
+                        }
+        }, completion: nil)
+    }
+
+}
---
+++ SliderView.swift
@@ -0,0 +1,131 @@
+//
+//  SliderView.swift
+//  https://github.com/ysoftware
+//
+//  Copyright © 2017 Yaroslav Erohin. All rights reserved.
+//
+
+import UIKit
+
+protocol SliderViewDelegate: class {
+    func sliderView(_ sliderView: SliderView, valueChanged value: Float)
+}
+
+/**
+Conform an object to this protocol and set SliderView's indicator property to receive slider value updates.
+*/
+protocol SliderViewIndicator {
+    var minValue:Float { get set }
+    var maxValue:Float { get set }
+    var value:Float { get set }
+}
+
+class SliderView:UIView {
+
+    // MARK: - Public
+
+    weak var delegate:SliderViewDelegate?
+    var indicator:SliderViewIndicator? {
+        didSet {
+            indicator?.minValue = minValue
+            indicator?.maxValue = maxValue
+            indicator?.value = value
+        }
+    }
+
+    /**
+    Controls whether slider should react to user's interactions. Default value is `true`. */
+    var isEnabled:Bool = true
+
+    /**
+    Controls how far should user swipe before slider starts to change value.
+    On lower values, this can be used to set precision of the slider updates.
+        
+    Measured in points. Default value is 0.
+    */
+    var resolution:Float = 0 {
+        didSet { if resolution > 0 { resolution = 0 }}
+    }
+
+    /**
+    Controls sensitivity to user's gestures.
+     
+    `1.0` means user has to swipe right through `100%` of view's size to reach maxValue from minValue.
+    Default value is 1.2.
+    */
+    var sensitivity:Float = 1.2 {
+        didSet { if sensitivity <= 0 { sensitivity = 0.01 }}
+    }
+
+    /** Lowest value of the slider. Cannot be higher than `maxValue`. Default value is 0.0. */
+    var minValue:Float = 0.0 {
+        didSet {
+            if minValue >= maxValue { minValue += 0.1 }
+            indicator?.minValue = minValue
+        }
+    }
+
+    /** Highest value of the slider. Cannot be lower than `minValue`. Default value is 1.0. */
+    var maxValue:Float = 1.0 {
+        didSet {
+            if minValue >= maxValue { maxValue -= 0.1 }
+            indicator?.maxValue = maxValue
+        }
+    }
+
+    /** Current value of the slider. Default value is 0.5. */
+    var value:Float = 0.5 {
+        didSet {
+            if value > maxValue { value = maxValue }
+            else if value < minValue { value = minValue }
+            indicator?.value = value
+        }
+    }
+
+    /** Middle point between `minValue` and `maxValue`. */
+    var middleValue:Float {
+        return (minValue + maxValue) / 2
+    }
+
+    /** Sets current `value` to middle point between `minValue` and `maxValue`. */
+    func setValueToMiddle() {
+        value = middleValue
+    }
+
+    // MARK: - Private
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    private func setup() {
+        backgroundColor = .clear
+
+        let recognizer = UIPanGestureRecognizer(target: self, action: #selector(viewPanned))
+        recognizer.maximumNumberOfTouches = 1
+        addGestureRecognizer(recognizer)
+    }
+
+    func viewPanned(_ sender: UIPanGestureRecognizer) {
+        guard isEnabled, sender.state == .changed else { return }
+
+        let translation = Float(sender.translation(in: self).x - sender.translation(in: self).y)
+        if abs(translation) >= resolution {
+            let dif = maxValue - minValue
+            let size = Float(min(frame.size.width, frame.size.height))
+            let panSize = size / sensitivity
+            let adaptation = panSize / dif
+
+            value += Float(translation / adaptation)
+            sender.setTranslation(.zero, in: sender.view)
+        }
+        delegate?.sliderView(self, valueChanged: value)
+    }
+
+}
